73c217b0b1be05825404df63e7d90404edc2e8f1
Guinea Wheek <guineawheek@gmail.com>
2018-08-19 20:14:45 -0700

    updated %plowie to mention a new feature

--- dozer/cogs/plowie.py
+++ dozer/cogs/plowie.py
@@ -52,7 +52,7 @@ class Plowie(Cog):
         e = discord.Embed(title="Plowie", description=f"Dozer for the masses (build )", color=discord.Color.blue())
         e.set_thumbnail(url=self.bot.user.avatar_url)
         e.add_field(name="About", value="Plowie is a fork of Dozer by the FRC Discord Development Team run by @guineawheek#5381, with a few extras tacked on and less stringent server requirements, making it suitable for personal servers. ")
-        e.add_field(name="Extra features", value="So far, Plowie offers `%afk`. This bot may occasionally get features before they are merged into upstream Dozer. ")
+        e.add_field(name="Extra features", value="So far, Plowie offers `%afk`, and the timed mutes/deafens subsystem is much more robust. This bot may occasionally get features before they are merged into upstream Dozer. ")
         e.add_field(name="Support", value="`%help` provides the general command reference; for special inquiries/feature requests contact @guineawheek#5381. Feature requests can be server specific.")
         e.add_field(name="Code", value="Check out the code [here!](https://github.com/guineawheek/Dozer/tree/plowie)")
         e.add_field(name="Invite link", value="Want to add Plowie to your server? [Click here!](https://discordapp.com/oauth2/authorize?client_id=474456308813266945&scope=bot&permissions=502656071)")
