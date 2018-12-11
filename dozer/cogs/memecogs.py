6c9bf37bd4e383a86d53d3a8139f2df8cbc7933c
Guinea Wheek <guineawheek@gmail.com>
2018-12-11 11:56:33 -0800

    fix up zihaocog a bit

--- dozer/cogs/memecogs.py
+++ dozer/cogs/memecogs.py
@@ -12,11 +12,13 @@ class OofboticsCog(MemeCog):
     guild_id = 433993458928058368
 
     async def on_message(self, msg):
-        if msg.guild.id != self.guild_id:
+        if not msg.guild or msg.guild.id != self.guild_id:
             return
         if msg.author.id == 434835322501726208:
+            moderation = self.bot.cogs["Moderation"]
             if random.randint(0, 50) == 14:
-                await self.bot.cogs["Moderation"]._mute(msg.author, reason="zihao", seconds=120, orig_channel=msg.channel)
+                if await moderation._mute(msg.author, reason="zihao", seconds=120, orig_channel=msg.channel):
+                    await moderation.mod_log(msg.guild.me, "muted", msg.author, "zihao", msg.channel, discord.Color.red())
 
 
 def setup(bot):
